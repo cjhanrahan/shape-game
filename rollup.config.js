08c81a39f8175983f47894acb9f74f03a4bc5b0b
Conor Hanrahan <cjhanrahan@gmail.com>
2018-02-17 15:08:08 -0800

    added eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
 
 export default {
-    input: 'main.js',
-    output: {
-        file: 'bundle.js',
-        format: 'iife',
-    },
-    plugins: [
-        babel({
-            exclude: 'node_modules/**'
-        }),
-        nodeResolve({
-            jsnext: true,
-        }),
-        commonjs({
-            include: 'node_modules/**',
-        }),
-        replace({
-            'process.env.NODE_ENV': 'development'
-        })
-    ],
+  input: 'main.js',
+  output: {
+    file: 'bundle.js',
+    format: 'iife',
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    nodeResolve({
+      jsnext: true,
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
+    replace({
+      'process.env.NODE_ENV': 'development',
+    }),
+  ],
 }
