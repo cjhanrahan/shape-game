10fffbc2bc135c569c364fad78fdace6fdb23c36
Conor Hanrahan <cjhanrahan@gmail.com>
2018-02-18 12:53:01 -0800

    got pyramid to show up

--- src/components/Pyramid.jsx
+++ src/components/Pyramid.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
-    BoxGeometry,
     CylinderGeometry,
     Mesh,
     MeshBasicMaterial,
@@ -29,20 +28,20 @@ export default class Pyramid extends React.Component {
         this.renderer = new WebGLRenderer({ canvas: this.canvas })
         this.renderer.setSize(this.canvas.offsetWidth, this.canvas.offsetHeight)
         this.addShape()
-        this.camera.z = 5
+        this.camera.position.z = 5
         this.boundAnimate()
     }
 
     addShape() {
-        // this.geometry = new CylinderGeometry(1, 1, 4)
-        this.geometry = new BoxGeometry(1, 1, 1)
-        this.material = new MeshBasicMaterial({ color: 0x4286f4 })
+        this.geometry = new CylinderGeometry(0, 1, 4, 4)
+        this.material = new MeshBasicMaterial({ color: 0x00ff00 })
         this.mesh = new Mesh(this.geometry, this.material)
         this.scene.add(this.mesh)
     }
 
     animate() {
         requestAnimationFrame(this.boundAnimate)
+        this.mesh.rotation.x += 0.1
         this.renderer.render(this.scene, this.camera)
     }
 
