767d65781ee7c25f1d4cb582ff2544f7cdb64260
Conor Hanrahan <cjhanrahan@gmail.com>
2025-02-02 22:47:34 -0800

    basic package working, about to do three

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      indent: ["error", 4],
+      semi: ["error", "never"],
+    },
+  },
 ];
 
 export default eslintConfig;
